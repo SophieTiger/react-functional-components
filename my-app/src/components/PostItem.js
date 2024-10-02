1cd371f1a1aabdc971cd3827611d04f85fb200a6
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-10-02 12:48:33 +0000

    Destruct props in PostItem.js

--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -3,14 +3,17 @@ import css from './css/PostItem.module.css'
 
 function PostItem(props) {
     return (
-        props.savedPosts.map(post => (
-            <div className={css.SearchItem} key={post.title}>
-                <p>{post.title}</p>
-                <p>{post.name}</p>
-                <img src={post.image} alt={post.title} />
-                <p>{post.description}</p>
+        props.savedPosts.map(post => {
+            // Extra task destructuring 
+            const {title, name, image, description} = post
+            return <div className={css.SearchItem} key={title}>
+                <p>{title}</p>
+                <p>{name}</p>
+                <img src={image} alt="random"/>
+                <p>{description}</p>
             </div>
-        ))
+            }
+        )
     )
 }
 
