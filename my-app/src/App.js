991a91c04e2d87d0b31ed613d28e7a1d13aeb098
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-10-02 09:32:24 +0000

    Add rendering list function component and a book child component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,15 @@ import css from './App.module.css';
 import Sidebar from "./components/Sidebar"
 import NavBarSimple from "./components/NavBarSimple"
 import NavBarForm from './components/NavBarForm';
+import RenderingLists from './components/RenderingLists';
 
 function App() {
   return (
     <div className={css.App}>
-      < Sidebar />
+      {/* < Sidebar /> */}
       {/* < NavBarSimple /> */}
-      <NavBarForm />
+      {/* <NavBarForm /> */}
+      <RenderingLists />
     </div>
   );
 }
